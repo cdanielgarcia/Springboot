e5620686294eed0b4c486efa67c984ee54a59ac2
Daniel Garcia <danielgarciaabello@gmail.com>
2021-06-17 22:00:21 -0500

    first commit

---
+++ src/test/java/com/iniciospring/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniciospring.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
